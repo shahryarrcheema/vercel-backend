2f2d0abd4fe3b70eb76acd1cfd8fec01269fe256
shahryarrcheema <shahryar1cheema@gmail.com>
2025-08-13 16:54:15 +0500

    new

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.get('/',(req ,res)=>{
+  res.send('server is running')
+})
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
